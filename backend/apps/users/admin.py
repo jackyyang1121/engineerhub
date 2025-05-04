7d835ec218e8b9fd56f2fbd916da00fcab45e9f1
jackyyang1121 <mingyongpig@gmail.com>
2025-05-05 02:26:30 +0800

    modify

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,8 @@
+# admin.py - 註冊 users app 的 models 進入 Django 後台管理介面
+# 可在此自訂管理介面顯示欄位、搜尋、過濾等功能
+
 from django.contrib import admin
 
-# Register your models here.
+# 目前尚未註冊任何 models，若要在後台管理 User，需加上：
+# from .models import User
+# admin.site.register(User)
